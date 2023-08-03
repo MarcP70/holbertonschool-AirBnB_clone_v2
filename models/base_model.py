1a14441071c8038dfd248f931f49091901a55bc2
Albaryys <6205@holbertonstudents.com>
2023-08-03 14:17:01 +0200

    add user and update base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-from logging.config import dictConfig
+
 import uuid
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
