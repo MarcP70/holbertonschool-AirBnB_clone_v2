93a0df5c1f786c4d9f091dd6b82a4e906ebcdca8
MarcP70 <marcpourias@yahoo.fr>
2023-08-03 16:12:57 +0200

    Fix: file city.py that fix a big

--- models/city.py
+++ models/city.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
-from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    class City(BaseModel, Base):
-        """ The city class, contains state ID and name """
-        __tablename__ = "cities"
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities")
 
-else:   # For FileStorage
-    class City(BaseModel):
-        """ The city class, contains state ID and name """
-        state_id = ""
-        name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id', ondelete="CASCADE"),
+                      nullable=False)
+    places = relationship("Place", cascade="all, delete", backref="cities",
+                          passive_deletes=True)
