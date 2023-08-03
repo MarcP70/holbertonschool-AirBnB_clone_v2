5491c60d8666b01e29dde60895d08a788493e670
MarcP70 <marcpourias@yahoo.fr>
2023-08-03 16:11:03 +0200

    Fix: file db_storage.py that fix bug

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,93 +1,85 @@
 #!/usr/bin/python3
-""" This file manage all the database """
-
+"""New engine DBStorage"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
 from os import getenv
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import (create_engine)
-from models.base_model import Base
-from models.state import State
-from models.city import City
-from models.user import User
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
 class DBStorage:
-    """ this class will contain all the manages of database
-    """
-
+    """class to store in MySQL"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """ this contain the definition of environ variables
-            the creation of engine and the reload that if
-            the test is equal to the environment should drop
-            down all the tables.
-        """
-
-        envi = getenv('HBNB_ENV')
-        my_user = getenv('HBNB_MYSQL_USER')
-        my_psswd = getenv('HBNB_MYSQL_PWD')
-        my_host = getenv('HBNB_MYSQL_HOST')
-        my_datab = getenv('HBNB_MYSQL_DB')
-
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(my_user, my_psswd,
-                                              my_host, my_datab),
-                                      pool_pre_ping=True)
-        self.reload()
-
-        if 'test' == envi:
-            Base.metadata.drop_all(self.__engine)
+        """Constructor method"""
+        from models.base_model import Base
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        db = getenv("HBNB_MYSQL_DB")
+        env = getenv("HBNB_ENV")
+        url = 'mysql+mysqldb://{}:{}@{}/{}'.format(user, passwd, host, db)
+        self.__engine = create_engine(url, pool_pre_ping=True)
+        if (env is not None and env == 'test'):
+            Base.metadata.drop_all(bind=self.__engine)
 
     def all(self, cls=None):
-        """ this contain the filter that depend of the class
-            that is specified
-        """
-        dicti = {}
-        list_cls = []
-
+        """Return a dictionary"""
+        from models.state import State
+        from models.city import City
+        from models.user import User
+        from models.place import Place
+        from models.review import Review
+        from models.amenity import Amenity
+        from models.base_model import Base
+        tables = {
+            'users': User,
+            'places': Place,
+            'states': State,
+            'cities': City,
+            'amenities': Amenity,
+            'reviews': Review
+        }
+        dict_entries = {}
         if cls is None:
-            list_cls += self.__session.query(State).all()
-            list_cls += self.__session.query(City).all()
-            list_cls += self.__session.query(User).all()
-            list_cls += self.__session.query(Place).all()
-            list_cls += self.__session.query(Review).all()
-            list_cls += self.__session.query(Amenity).all()
-
+            for base_class in tables.values():
+                for row in self.__session.query(base_class).all():
+                    dict_entries['{}.{}'
+                                 .format(base_class.__name__, row.id)] = row
         else:
-            list_cls = self.__session.query(cls).all()
-
-        for var in list_cls:
-            k = type(var).__name__ + '.' + var.id
-            dicti[k] = var
-
-        return dicti
+            for row in self.__session.query(cls):
+                dict_entries['{}.{}'
+                             .format(cls.__name__, row.id)] = row
+        return dict_entries
 
     def new(self, obj):
-        """ add the object to session """
-
+        """Add an object"""
         self.__session.add(obj)
 
     def save(self):
-        """ commit all the changes to session """
-
+        """Commit the change"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """ delete from the session """
-
-        if obj:
+        """Delete an object"""
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """ This function create all the tables and the session """
+        """Create the session"""
+        from models.state import State
+        from models.city import City
+        from models.user import User
+        from models.place import Place
+        from models.review import Review
+        from models.amenity import Amenity
+        from models.base_model import Base
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine,
-                                                     expire_on_commit=False))()
+        Session = scoped_session(sessionmaker(bind=self.__engine,
+                                              expire_on_commit=False))
+        self.__session = Session()
 
     def close(self):
-        """ This function close engines"""
+        """Close the session"""
         self.__session.close()
