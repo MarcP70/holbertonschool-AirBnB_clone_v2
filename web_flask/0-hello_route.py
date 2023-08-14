456120f00a375d764cdda1fa0ceb283e42443238
MarcP70 <marcpourias@yahoo.fr>
2023-08-14 10:11:15 +0200

    Add: file 0-hello_route.py and the __init__.py file that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,8 +7,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Handle HTTP requests to the root URL ("/") and return a greeting message.
+
+    Returns:
+        str: A greeting message.
+
+    """
     return "Hello HBNB!"
 
 
