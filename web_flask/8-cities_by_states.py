0abd884a80af185cd8c292650c9008d490e5ed15
MarcP70 <marcpourias@yahoo.fr>
2023-08-14 16:06:45 +0200

    Add: files 8-cities_by_states.py and .html that starts a Flask web application to list cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+    Retrieve all states from the database, sort them alphabetically by name,
+    and render a template with the states and their associated cities.
+
+    Returns:
+        str: Rendered template with states and cities.
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """
+    Closes the session used by the Flask application.
+
+    :param exception: The exception that occurred during the teardown process,
+        if any.
+    :return: None
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
