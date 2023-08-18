9989002a1fd3235ef9bcc315f18d7ced641361f6
MarcP70 <marcpourias@yahoo.fr>
2023-08-18 12:16:13 +0200

    Fix: files 9-states.py and .html

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,41 +4,25 @@ Starts a Flask web application.
 """
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 
-
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
-
-
+@app.route("/states/", strict_slashes=False)
 @app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
-
+def states(id=None):
+    states = storage.all(State).values()
+    if id is not None:
+        for state in states:
+            if state.id == id:
+                return render_template('9-states.html', states=state)
+        return render_template('9-states.html')
+    return render_template('9-states.html', states=states, full=True)
 
 @app.teardown_appcontext
-def close_session(exception):
-    """
-    Closes the session used by the Flask application.
-
-    :param exception: The exception that occurred during the teardown process,
-        if any.
-    :return: None
-    """
+def teardown(self):
+    """Closes the storage"""
     storage.close()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
