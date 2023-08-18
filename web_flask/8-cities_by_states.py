48a1247cef15233d7605600a9cc80644b9847da4
MarcP70 <marcpourias@yahoo.fr>
2023-08-18 13:36:13 +0200

    Fix: file 8-cities_by_states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-"""
-from flask import Flask, render_template
+
+"""This is a script thats starts a Flask web applications"""
+
 from models import storage
 from models.state import State
+from flask import Flask, render_template
 
-app = Flask(__name__)
+blog_app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
+@blog_app.route('/states_list', strict_slashes=False)
+def states_list():
+    return render_template('7-states_list.html', states_li=storage.all(State))
+
+
+@blog_app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """
-    Retrieve all states from the database, sort them alphabetically by name,
-    and render a template with the states and their associated cities.
-
-    Returns:
-        str: Rendered template with states and cities.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    return render_template("/8-cities_by_states.html", sts=storage.all(State))
 
 
-@app.teardown_appcontext
-def teardown(self):
-    """Closes the storage"""
+@blog_app.teardown_appcontext
+def tear_down(self):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    blog_app.run(host='0.0.0.0', port='5000')
